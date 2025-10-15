26fe05439a0649001702f1792a3c8a5fd4c1b25c
Trưởng Làng Quân <kinghero1503@gmail.com>
2025-10-15 12:38:32 +0700

    Update oldroot.py

--- oldroot.py
+++ oldroot.py
@@ -1,25 +1,47 @@
 import string
 import tkinter
+import python_library 
 
-stringlower = string.ascii_lowercase
-stringupper = string.ascii_uppercase
-
-def ceasar(text:str,shift:int):
+def ceasar(text:str,shift:int,mode:int):
     result = ''
-    shift %= 26
-    for i in range(len(text)):
-        if text[i] in stringupper:
-            result += chr(((ord(text[i] - ord('A') + shift)) % 26) + ord("A"))
-        elif text[i] in stringlower:
-            result += chr(((ord(text[i]) - ord('a') + shift) % 26) + ord('a'))
+
+    if mode == 2:
+        shift = -shift
+
+    for i,ch in enumerate(text):
+        if ch.isalpha():
+            is_upper = ch.isupper()
+
+            base = ord(ch) - ord("A") if is_upper else ord("a")
+            after = (ord(ch) - base + shift) % 26
+            result += after
         else:
-            result += text[i]  
+            result += ch
     return result
 
-def ceasard(text:str,shift:int):
-    result += ceasar(text, -shift)
+def vigenere(text:str,key:str,mode:int):
+    result = ''
+
+    for i,ch in enumerate(text):
+        if ch.isalpha():
+            is_upper = ch.isupper()
+
+            base = ord(ch) - ord("A") if is_upper else ord("a")
+            key_char = key[i % len(key)].lower()
+            shift = ord(key_char) - ord("a")
+            
+            if mode == 2:
+                shift = -shift
+
+            result += chr((ord(ch) - base + shift) % 26 + base)
+
+        else:
+            result += ch
     return result
 
-def vigenere(text:str,key:int):
-    pass
+def aibash(text:str,mode:int):
+    result = ''
 
+    for i,ch in enumerate(text):
+        if ch.isalpha():
+            pass
